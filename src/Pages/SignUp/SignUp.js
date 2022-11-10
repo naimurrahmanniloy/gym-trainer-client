af32069817ac070d1c734ab3c3de39842bca30e3
naimurrahmanniloy <niloyrahman432@gmail.com>
2022-11-10 11:37:40 +0600

    sign up done

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import img from '../../assets/login.jpg'
+import { FcGoogle } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+
+    const handleSignUp = event => {
+        event.preventDefault();
+    }
+
+    return (
+        <div className="hero h-1/2 w-full bg-base-200 my-6">
+            <div className="hero-content gap-14 grid md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+
+                    <img src={img} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full  shadow-2xl p-10 bg-base-100 py-10">
+                    <h1 className="text-5xl text-center font-bold">Sign Up Now!</h1>
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" required />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="SignUp" />
+
+                        </div>
+                        <p className='text-center text-lg'>Or sign in with</p>
+                        <div className="form-control mt-6">
+                            <button className=' btn btn-primary'>Google <FcGoogle className='ml-3 text-2xl'></FcGoogle></button>
+
+                        </div>
+                    </form>
+                    <p className='text-center'>Hey!! Already have an account? <Link className='text-lg font-bold' to='/login'>Login</Link></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
